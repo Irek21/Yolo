b3764c8265b25a89b33ef62677c13ad5c87abbcc
Irek21 <55410823+Irek21@users.noreply.github.com>
2021-08-15 09:52:10 +0300

    Delete data.py

--- data.py
+++
@@ -1,9 +0,0 @@
-import torchvision
-
-prepr = torchvision.transforms.Compose([
-    torchvision.transforms.Resize((224, 224)),
-    torchvision.transforms.ToTensor()
-])
-voc = torchvision.datasets.VOCDetection(root='data/train',
-                                        image_set='train',
-                                        transform=prepr)
